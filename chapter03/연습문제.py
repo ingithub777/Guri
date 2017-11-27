4198b877cae12eed1bab1c2910946202053426bc
IT-4 <you@example.com>
2017-11-27 16:42:32 +0900

    a

---
+++ chapter03/연습문제.py
@@ -0,0 +1,15 @@
+#Q1
+#
+#i=0
+#while True:
+#    i= i + 1
+#    if i>5: break
+#    print('*'*i)
+
+#02
+#a=[70,60,55,75,95,90,80,80,85,100]
+#total=0
+#for b in a:
+#    total=total+b
+#average=total/10
+#print(average)
