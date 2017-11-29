8b3a19cdeb3fedac55175e6a400705ee04f03e35
IT-4 <you@example.com>
2017-11-29 11:47:18 +0900

    a

--- inhan/문제5.py
+++ inhan/문제5.py
@@ -1,17 +1,14 @@
-old=0
 money=0
-card=0
 fticket = 3
 dticket = 5
-ticket = 0
 while True:
-    old=int(input("나이를 알려주세요: "))
-    if old <=3:
+    age=int(input("나이를 입력하세요: "))
+    if age <=3:
          print("귀하는 유아 등급이며, 무료입니다.")
-    elif 4 <= old <= 13:
+    elif 4 <= age <= 13:
         print("귀하는 어린이 등급이며, 2000원입니다.")
-        pey=int(input("1: 현금, 2: 카드 선택해주세요 : "))
-        if pey == 1:
+        pay=int(input("1: 현금, 2: 카드 선택해주세요 : "))
+        if pay == 1:
             money = int(input("요금을 넣어주세요: "))
             if money == 2000:
                 print("감사합니다. 티켓을 발행합니다.")
@@ -33,7 +30,7 @@ while True:
                     print("축하합니다. 연간회원권 구매 이벤트에 당첨되셨습니다.연간 회원 할인 티켓을 발행합니다. 잔여 무료티켓", [dticket], "장")
             elif money < 2000:
                 print(2000-money, "가 모자랍니다." "입력하신",money,"를 반환합니다.")
-        elif pey == 2:
+        elif pay == 2:
             print(2000*0.9,"원 결제되었습니다. 티켓을 발행합니다.")
             ticket = ticket + 1
             if ticket % 7 == 0:
@@ -42,10 +39,10 @@ while True:
             if ticket % 4 == 0:
                 dticket = dticket-1
                 print("축하합니다. 연간회원권 구매 이벤트에 당첨되셨습니다.연간 회원 할인 티켓을 발행합니다. 잔여 무료티켓",[dticket],"장")
-    elif 14 <= old <= 18:
+    elif 14 <= age <= 18:
         print("귀하는 청소년 등급이며, 3000원입니다.")
-        pey=int(input("1: 현금, 2: 카드 선택해주세요 : "))
-        if pey == 1:
+        pay=int(input("1: 현금, 2: 카드 선택해주세요 : "))
+        if pay == 1:
             money = int(input("요금을 넣어주세요: "))
             if money == 3000:
                 print("감사합니다. 티켓을 발행합니다.")
@@ -67,7 +64,7 @@ while True:
                     print("축하합니다. 연간회원권 구매 이벤트에 당첨되셨습니다.연간 회원 할인 티켓을 발행합니다. 잔여 무료티켓", [dticket], "장")
             elif money < 3000:
                 print(3000-money, "가 모자랍니다." "입력하신",money,"를 반환합니다.")
-        elif pey == 2:
+        elif pay == 2:
             print(3000*0.9,"원 결제되었습니다. 티켓을 발행합니다.")
             ticket = ticket+1
             if ticket % 7 == 0:
@@ -76,10 +73,10 @@ while True:
             if ticket % 4 == 0:
                 dticket = dticket-1
                 print("축하합니다. 연간회원권 구매 이벤트에 당첨되셨습니다.연간 회원 할인 티켓을 발행합니다. 잔여 무료티켓",[dticket],"장")
-    elif 19 <= old <= 59:
+    elif 19 <= age <= 59:
         print ("귀하는 성인 등급이며, 5000원입니다.")
-        pey = int(input("1: 현금, 2: 카드 선택해주세요 : "))
-        if pey == 1:
+        pay = int(input("1: 현금, 2: 카드 선택해주세요 : "))
+        if pay == 1:
             money = int(input("요금을 넣어주세요: "))
             if money == 5000:
                 print("감사합니다. 티켓을 발행합니다.")
@@ -101,7 +98,7 @@ while True:
                     print("축하합니다. 연간회원권 구매 이벤트에 당첨되셨습니다.연간 회원 할인 티켓을 발행합니다. 잔여 무료티켓", [dticket], "장")
             elif money < 5000:
                 print(5000-money, "가 모자랍니다." "입력하신",money,"를 반환합니다.")
-        elif pey == 2:
+        elif pay == 2:
             print(5000*0.9,"원 결제되었습니다. 티켓을 발행합니다.")
             ticket = ticket+1
             if ticket % 7 == 0:
@@ -110,10 +107,10 @@ while True:
             if ticket % 4 == 0:
                 dticket = dticket-1
                 print("축하합니다. 연간회원권 구매 이벤트에 당첨되셨습니다.연간 회원 할인 티켓을 발행합니다. 잔여 무료티켓",[dticket],"장")
-    elif 60 <= old <= 65:
+    elif 60 <= age <= 65:
         print("귀하는 성인 등급이며, 5000원입니다.")
-        pey = int(input("1: 현금, 2: 카드 선택해주세요 : "))
-        if pey == 1:
+        pay = int(input("1: 현금, 2: 카드 선택해주세요 : "))
+        if pay == 1:
              money = int(input("요금을 넣어주세요: "))
              if money == 5000:
                 print("감사합니다. 티켓을 발행합니다.")
@@ -135,7 +132,7 @@ while True:
                     print("축하합니다. 연간회원권 구매 이벤트에 당첨되셨습니다.연간 회원 할인 티켓을 발행합니다. 잔여 무료티켓", [dticket], "장")
              elif money < 5000:
                     print(5000 - money, "가 모자랍니다." "입력하신", money, "를 반환합니다.")
-        elif pey == 2:
+        elif pay == 2:
             print(5000*0.85,"원 결제되었습니다. 티켓을 발행합니다.")
             ticket = ticket+1
             if ticket % 7 == 0:
@@ -144,5 +141,5 @@ while True:
             if ticket % 4 == 0:
                 dticket = dticket-1
                 print("축하합니다. 연간회원권 구매 이벤트에 당첨되셨습니다.연간 회원 할인 티켓을 발행합니다. 잔여 무료티켓",[dticket],"장")
-    elif old >= 66:
+    elif age >= 66:
             print ("귀하는 노인 등급이며, 무료.")
