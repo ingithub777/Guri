db090861bd8fb7ba6e10d1200ac21b52890d2781
rladlsgks4 <rladlsgks4@naver.com>
2017-12-11 17:28:34 +0900

    a

---
+++ chapter05/render_test.py
@@ -0,0 +1,12 @@
+# from game.sound.echo import echo_test
+# def render_test():
+#      print("render")
+#      echo_test()
+#
+from game.graphic.render import render_test
+render_test()
+
+# from ..sound.echo import echo_test
+#
+# def render_test():
+#     print
