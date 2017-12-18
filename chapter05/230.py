33dacb4d0d18757f75b2c84dc40107a481062d57
ingithub777 <ingithub@naver.com>
2017-12-18 23:13:30 +0900

    a

--- chapter05/230.py
+++ chapter05/230.py
@@ -1,32 +1,32 @@
-def check_number():
-    if(number<0):
-        raise ValueError
-
-try:
-    number = int(input("양수를 입력하세요"))
-    check_number()
+# def check_number():
+#     if(number<0):
+#         raise ValueError
+#
+# try:
+#     number = int(input("양수를 입력하세요"))
+#     check_number()
         # print("양수를 입력하세요.")
-        raise ValueError
+        # raise ValueError
     # f=open("나없는파일",'r')
     # 4/0
-    print("hello world")
+    # print("hello world")
 # except:
 
-except FileNotFoundError as e:
-    print("없는 파일을 열었습니다.")
-    print("시스템 에러 메세지: "+str(e))
-    print(e)
-
-except ZeroDivisionError:
+# except FileNotFoundError as e:
+#     print("없는 파일을 열었습니다.")
+#     print("시스템 에러 메세지: "+str(e))
+#     print(e)
+#
+# except ZeroDivisionError:
     # print("에러가 발생했습니다.")
-      print("0으로 나누었습니다.")
+    #   print("0으로 나누었습니다.")
 
-except ValueError as e:
-    print("음수를 입력했네요.")
+# except ValueError as e:
+#     print("음수를 입력했네요.")
 
-else:
-    print("Thank You!!")
+# else:
+#     print("Thank You!!")
 
-finally:
-    print("see ya later")
-print("프로그램 정상 종료")
+# finally:
+#     print("see ya later")
+# print("프로그램 정상 종료")
--- inhan/문제29.py
+++ inhan/문제29.py
@@ -1,21 +1,29 @@
-e2 = 0
-while True:
-    a = input("범위, 두 수를 입력하세요 : ").split(' ')
-    b2 = []
-    c2 = []
-    for i in range(1,int(a[0])):
-        b = int(a[1])*i
-        c = int(a[2])*i
-        d = b2+c2
-        d2 = set(d)
-        b2.append(b)
-        c2.append(c)
-        e = list(d2)
-        for k in e:
-            e2 += k
-    print("""0부터 %s 이하의 범위를 선택하셨습니다. 이 중에서
-             %s의 배수는 %s 입니다.
-             %s의 배수는 %s 입니다.
-             %s와 %s의 배수는 %s입니다.
-             따라서 0부터 %s이하의 범위내에서 %s와 %s의 배수의 총합은 %s입니다.""" % (a[0], a[1], b2, a[2], c2, a[1], a[2], d2, a[0], a[1], a[2], e2))
-    break
+list_number=input('3개의 값을 입력하세요.:').split()
+## 15 3 5
+
+first_num=[]
+second_num=[]
+list_number[0]=int(list_number[0])
+list_number[1]=int(list_number[1])
+list_number[2]=int(list_number[2])
+
+for i in range(1,list_number[0]+1):
+    if i * list_number[1] <= list_number[0]:
+        first_num.append(i*list_number[1])
+    if i * list_number[2] <= list_number[0]:
+        second_num.append(i*list_number[2])
+
+# sum=set(first_num) | set(second_num)
+first_num=set(first_num)
+second_num=set(second_num)
+
+sum=first_num | second_num
+sum=list(sum)
+
+num=0
+for i in sum:
+    num+=i
+print("0부터 %s 이하의 범위를 선택하셨습니다. 이중에서" % list_number[0])
+print("%s의 배수는 %s 입니다." % (list_number[1], first_num))
+print("%s의 배수는 %s 입니다." % (list_number[2], second_num))
+print("따라서 0부터 %s 이하의 범위내에서 %s 와 %s의 배수의 총합은 %d입니다." % (list_number[0], list_number[1], list_number[2], num))
