a016bbbfb4914766b83874a9a5a90bfac313100f
a <ingithub@naver.com>
2018-01-09 22:14:35 +0900

    a

--- 코딩도장/복습.py
+++ 코딩도장/복습.py
@@ -1,6 +1,8 @@
+numbers = int(input('입력할 정수의 개수를 입력하세요 : '))
 sum = 0
-for hour in range(24):
-    for minute in range(60):
-        if "3" in str(hour) or "3" in str(minute):
-            sum += 60
-print(sum)
+
+for i in range(numbers):
+    sum += int(input('정수를 입력하세요 : '))
+
+print('입력한 정수의 합은 :', sum, '입니다.')
+print('입력한 정수의 평균은 :', sum/numbers, '입니다.')
