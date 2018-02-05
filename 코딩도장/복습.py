5ea028eb627e8b56837dea5595354baa83aebd7a
a <ingithub@naver.com>
2018-02-05 20:29:21 +0900

    a

--- 코딩도장/복습.py
+++ 코딩도장/복습.py
@@ -1,6 +1,48 @@
-import math
+# 1부터 10까지의 숫자 중 5를 제외한 나머지를 순서대로 출력하세요.
+# (2가지 이상의 다른 방법으로 풀어보세요.)
 
-m = int(input('총건수: '))
-n = int(input('한페이지에 보여줄 게시물수: '))
+# for i in range(1,5):
+#     print(i)
+# for i in range(6,11):
+#     print(i)
 
-print(math.ceil(m/n))
+# a = []
+# for i in range(1,11):
+#     a.append(i)
+# b = a[0:4]+a[5:10]
+# print(b)
+
+# a = []
+# for i in range(1,11):
+#     a.append(i)
+# a.remove(5)
+# print(a)
+
+# for i in range(1,11):
+#    if i==5:
+#       i+=1
+#    print(i)
+
+# 난이도 최하
+#
+# 사각형을 그리세요
+#
+# ex)입력 : 3
+#
+# ***
+# * *
+# ***
+#
+# 입력 : 5
+#
+# *****
+# *   *
+# *   *
+# *   *
+# *****
+
+a = input("숫자를 입력하세요: ")
+for i in a:
+    i = int(i)
+    i = '*'*int(a)
+    print(i)
